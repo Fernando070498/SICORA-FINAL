38de7040f09c37065ba17b73c7808da97b2ce2b0
MC FR <paolo_cueva@usmp.pe>
2018-09-22 19:43:03 -0500

    version1.3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,22 +10,8 @@ namespace SICORA.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Principal()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
@@ -33,9 +19,6 @@ namespace SICORA.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Principal()
-        {
-            return View();
-        }
+        
     }
 }
